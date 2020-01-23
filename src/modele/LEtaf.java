bfc06af44236b791e9be257cb9d2b13c1ee28fee
urschfeld <urschfeld@localhost>
2020-01-23 14:41:09 +0100

    ceci est le commit

---
+++ src/modele/LEtaf.java
@@ -0,0 +1,19 @@
+package modele;
+
+public class LEtaf {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=10;i++) {
+			System.out.println("pierre tu soule");
+			
+			System.out.println(i*5);
+		}
+		
+		
+		
+		
+
+	}
+
+}
